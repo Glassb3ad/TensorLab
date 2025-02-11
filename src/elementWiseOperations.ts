9994696512d089fced429a44d2aec272cb7e53fb
GlassB3ad <j.lahtivirta@gmail.com>
2025-02-11 20:22:28 +0200

    get 0 when treshold is 0.5 and scalar 0.2

---
+++ src/elementWiseOperations.ts
@@ -0,0 +1,4 @@
+export const treshold =
+  (tresholdVal: number, aboveTreshold = 1, underTreshold = 0) =>
+  (scalar: number): number =>
+    0;
---
+++ tests/elementWiseOperations.test.ts
@@ -0,0 +1,12 @@
+import { expect, test, describe } from 'vitest';
+import { treshold } from '../src/elementWiseOperations';
+
+describe('Element-wise methods', () => {
+  describe('Treshold', () => {
+    test('Get 0 when treshold is 0.5 and scalar 0.2', () => {
+      const tresholdFunc = treshold(0.5);
+      const scalar = tresholdFunc(0.2);
+      expect(scalar).toBe(0);
+    });
+  });
+});
