4ec4d427ef446e009f030d2b1606a2175e6cfb6d
GlassB3ad <j.lahtivirta@gmail.com>
2025-02-11 20:55:14 +0200

    rename function

--- src/elementWiseOperations.ts
+++ src/elementWiseOperations.ts
@@ -1,4 +1,4 @@
-export const treshold =
+export const createTresholdOperation =
   (tresholdVal: number, aboveTreshold = 1, underTreshold = 0) =>
   (scalar: number): number =>
     scalar >= tresholdVal ? aboveTreshold : underTreshold;
--- tests/elementWiseOperations.test.ts
+++ tests/elementWiseOperations.test.ts
@@ -1,30 +1,32 @@
 import { expect, test, describe } from 'vitest';
-import { treshold } from '../src/elementWiseOperations';
+import { createTresholdOperation } from '../src/elementWiseOperations';
 
 describe('Element-wise methods', () => {
   describe('Treshold', () => {
-    test('Get 0 when treshold is 0.5 and scalar 0.2', () => {
-      const tresholdFunc = treshold(0.5);
-      const scalar = tresholdFunc(0.2);
-      expect(scalar).toBe(0);
-    });
+    describe('treshold operation', () => {
+      test('Get 0 when treshold is 0.5 and scalar 0.2', () => {
+        const tresholdOperation = createTresholdOperation(0.5);
+        const scalar = tresholdOperation(0.2);
+        expect(scalar).toBe(0);
+      });
 
-    test('Get 1 when treshold is 0.5 and scalar 0.7', () => {
-      const tresholdFunc = treshold(0.5);
-      const scalar = tresholdFunc(0.7);
-      expect(scalar).toBe(1);
-    });
+      test('Get 1 when treshold is 0.5 and scalar 0.7', () => {
+        const tresholdOperation = createTresholdOperation(0.5);
+        const scalar = tresholdOperation(0.7);
+        expect(scalar).toBe(1);
+      });
 
-    test('Get aboveTreshold value when treshold is 0.5 and scalar 0.7', () => {
-      const tresholdFunc = treshold(0.5, 0.99);
-      const scalar = tresholdFunc(0.7);
-      expect(scalar).toBe(0.99);
-    });
+      test('Get aboveTreshold value when treshold is 0.5 and scalar 0.7', () => {
+        const tresholdOperation = createTresholdOperation(0.5, 0.99);
+        const scalar = tresholdOperation(0.7);
+        expect(scalar).toBe(0.99);
+      });
 
-    test('Get underTreshold value when scalar is under treshold', () => {
-      const tresholdFunc = treshold(0.5, 0.99, 0.11);
-      const scalar = tresholdFunc(0.2);
-      expect(scalar).toBe(0.11);
+      test('Get underTreshold value when scalar is under treshold', () => {
+        const tresholdOperation = createTresholdOperation(0.5, 0.99, 0.11);
+        const scalar = tresholdOperation(0.2);
+        expect(scalar).toBe(0.11);
+      });
     });
   });
 });
