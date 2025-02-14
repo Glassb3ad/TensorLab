5a782c97999b12062ae7c781a7e6780729de64aa
GlassB3ad <j.lahtivirta@gmail.com>
2025-02-14 20:14:12 +0200

    move convolution to its own file

--- src/tensor.ts
+++ src/tensor.ts
@@ -55,39 +55,3 @@ export const dotProduct = (t1: Tensor, t2: Tensor): Tensor => {
   }
   return dotProductRec(t1, t2);
 };
-
-export const sliceTensorByKernel = (t1: Tensor, kernel: Tensor): Array<Tensor> => {
-  if (isScalar(t1) || isScalar(kernel)) {
-    throw new Error('Scalar cannot be sliced');
-  }
-  const sliceLength = kernel.length;
-  const sliceCount = t1.length - sliceLength + 1;
-  return [...Array(sliceCount).keys()].map(i => t1.slice(i, sliceLength + i));
-};
-
-const vectorConvolution = (tensor: Tensor, kernel: Tensor): Tensor => {
-  const slices = sliceTensorByKernel(tensor, kernel);
-  return slices.map(slice => dotProduct(slice, kernel));
-};
-
-export const convolution = (tensor: Tensor, kernel: Tensor): Tensor => {
-  if (isScalar(tensor) || isScalar(kernel)) {
-    throw new Error('Convolution does not support scalars');
-  }
-
-  if (isVector(tensor) && isVector(kernel)) {
-    return vectorConvolution(tensor, kernel);
-  }
-
-  const slices = sliceTensorByKernel(tensor, kernel);
-  const convolutedTensors = [];
-  for (const slice of slices) {
-    const tempConvolutions = [];
-    for (let i = 0; i < kernel.length; i++) {
-      const subKernel = kernel[i];
-      tempConvolutions.push(convolution((slice as Array<Tensor>)[i], subKernel));
-    }
-    convolutedTensors.push(tempConvolutions.reduce((pre, cur) => add(pre, cur)));
-  }
-  return convolutedTensors;
-};
