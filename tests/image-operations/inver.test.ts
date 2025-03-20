79be979296e571c4b0f408592e95bf110d91926e
GlassB3ad <j.lahtivirta@gmail.com>
2025-03-20 20:36:59 +0200

    use property based testing to check that All scalars become max - scalar

--- tests/image-operations/inver.test.ts
+++ tests/image-operations/inver.test.ts
@@ -1,9 +1,11 @@
 import { expect, test, describe } from 'vitest';
 import { Tensor } from '../../src/tensor';
 import { invert } from '../../src/image-operations/invert';
+import fc from 'fast-check';
+import { max } from '../../src/recursive-operations';
 
 describe('invert', () => {
-  test('All scalars become max - scalar', () => {
+  test('All scalars become 8 - scalar when 8 is max value in tensor', () => {
     const matrix: Tensor = [
       [2, 2],
       [3, 3],
@@ -15,5 +17,18 @@ describe('invert', () => {
       [5, 5],
       [0, 4],
     ]);
+  });
+
+  test('All scalars become max - scalar', () => {
+    fc.assert(
+      fc.property(fc.array(fc.integer({ min: 0, max: 255 })), vector => {
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const maxValue = max(vector)!;
+        const inverted = invert(vector);
+        vector.forEach((element, i) => {
+          expect(element).toBe(maxValue - (inverted as Array<number>)[i]);
+        });
+      }),
+    );
   });
 });
