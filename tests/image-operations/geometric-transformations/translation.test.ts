372c3548b13b5f075a7a628c4f22f3e45d772295
GlassB3ad <j.lahtivirta@gmail.com>
2025-03-24 12:20:21 +0200

    implement geometric translation

---
+++ tests/image-operations/geometric-transformations/translation.test.ts
@@ -0,0 +1,33 @@
+import { expect, test, describe } from 'vitest';
+import { Tensor } from '../../../src/tensor';
+import { translate } from '../../../src/image-operations/geometric-transformation/translation';
+
+describe('translation', () => {
+  test('move one right-down', () => {
+    const matrix: Tensor = [
+      [1, 2],
+      [3, 4],
+      [5, 6],
+    ];
+    const transformedMatrix = translate(matrix, 1, 1);
+    expect(transformedMatrix).toEqual([
+      [0, 0],
+      [0, 1],
+      [0, 3],
+    ]);
+  });
+
+  test('move one left-up', () => {
+    const matrix: Tensor = [
+      [1, 2],
+      [3, 4],
+      [5, 6],
+    ];
+    const transformedMatrix = translate(matrix, -1, -1);
+    expect(transformedMatrix).toEqual([
+      [4, 0],
+      [6, 0],
+      [0, 0],
+    ]);
+  });
+});
