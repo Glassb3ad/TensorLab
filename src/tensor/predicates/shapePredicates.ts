1b656a4ba644ab8d341a9eb097e73157644dcc8b
GlassB3ad <j.lahtivirta@gmail.com>
2025-03-26 23:46:42 +0200

    improve typing

--- src/tensor/predicates/shapePredicates.ts
+++ src/tensor/predicates/shapePredicates.ts
@@ -1,12 +1,12 @@
-import { Tensor } from '@tensor/types';
+import { Scalar, Tensor } from '@tensor/types';
 
-export const isScalar = (t: Tensor) => typeof t === 'number';
+export const isScalar = (t: Tensor): t is Scalar => typeof t === 'number';
 
-export const isVector = (t: Tensor) => !isScalar(t) && isScalar(t[0]);
+export const isVector = (t: Tensor): t is Array<Scalar> => !isScalar(t) && isScalar(t[0]);
 
-export const isMatrix = (t: Tensor): t is Array<Array<Tensor>> => !isScalar(t) && isVector(t[0]);
+export const isMatrix = (t: Tensor): t is Array<Array<Scalar>> => !isScalar(t) && isVector(t[0]);
 
-export const is3D = (t: Tensor) => !isScalar(t) && isMatrix(t[0]);
+export const is3D = (t: Tensor): t is Array<Array<Array<Scalar>>> => !isScalar(t) && isMatrix(t[0]);
 
 export const haveEqualShape = (t1: Tensor, t2: Tensor): boolean => {
   if (isScalar(t1) && isScalar(t2)) {
