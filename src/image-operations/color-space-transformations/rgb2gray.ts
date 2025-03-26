bb19eec16b4f446733bdd388f2c5f9c57f107793
GlassB3ad <j.lahtivirta@gmail.com>
2025-03-26 22:01:49 +0200

    use arrow functions

--- src/image-operations/color-space-transformations/rgb2gray.ts
+++ src/image-operations/color-space-transformations/rgb2gray.ts
@@ -4,10 +4,10 @@ import { Tensor } from '@tensor/types';
 
 const RGB_2_GRAY_KERNEL = [[[0.2989]], [[0.587]], [[0.114]]];
 
-export function rgb2gray(tensor: Tensor) {
+export const rgb2gray = (tensor: Tensor) => {
   if (!is3D(tensor)) {
     throw Error('Tensor is not 3 dimensional');
   }
   const convolutedTensor = convolution(tensor, RGB_2_GRAY_KERNEL) as Array<Tensor>;
   return convolutedTensor[0];
-}
+};
