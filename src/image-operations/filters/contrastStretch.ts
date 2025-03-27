1f916ddef4f36d236d9f7a88d4be09d28cf461af
GlassB3ad <j.lahtivirta@gmail.com>
2025-03-27 13:09:11 +0200

    min always returns number

--- src/image-operations/filters/contrastStretch.ts
+++ src/image-operations/filters/contrastStretch.ts
@@ -4,9 +4,9 @@ import { max } from '@tensor/properties/max';
 import { min } from '@tensor/properties/min';
 import { tensorGuard } from '@tensor/tensorGuard';
 
-const contrastStretchRaw = (image: Tensor, globalMax: number, defaultMin = 0) => {
+const contrastStretchRaw = (image: Tensor, globalMax: number) => {
   const maxVal: number = max(image);
-  const minVal: number = min(image) ?? defaultMin;
+  const minVal: number = min(image);
   // ensures that divider is not zero
   const divider = maxVal - minVal || 1;
   const pointwiseOperation = (pixel: number) => ((pixel - minVal) / divider) * globalMax;
